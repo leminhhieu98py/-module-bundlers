a951afa1903b9e5939a3e47ce24e79c7620c4b89
Otis <minhhieu.le@navercorp.com>
2024-05-15 10:35:06 +0700

    webpack: getting started completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { join } from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, now imported by this script
+  element.innerHTML = join(['Hello', 'webpack', 'from Otiss'], ' ');
 
   return element;
 }
