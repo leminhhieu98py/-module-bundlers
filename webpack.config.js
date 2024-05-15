035a466633e9e5984e6e41e3da31ef35cdd9662c
Otis <minhhieu.le@navercorp.com>
2024-05-15 14:41:57 +0700

    webpack: output management completed

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const toml = require('toml');
 const yamljs = require('yamljs');
 const json5 = require('json5');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const fontRegex = /\.(woff|woff2|eot|ttf|otf)$/i;
+const imageRegex = /\.(png|svg|jpg|jpeg|gif)$/i;
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [
       // Asset management
@@ -17,11 +18,11 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: imageRegex,
         type: 'asset/resource'
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: fontRegex,
         type: 'asset/resource'
       },
       {
@@ -54,5 +55,21 @@ module.exports = {
         }
       }
     ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Output management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: ({ filename }) => {
+      if (fontRegex.test(filename)) return 'fonts/[name][ext]';
+      if (imageRegex.test(filename)) return 'images/[name][ext]';
+
+      return 'asset/[name][ext]';
+    },
+    clean: true
   }
 };
