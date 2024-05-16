443af44d5456f62c3b1bc0cf81147ce0f9215a8a
leminhhieu98py <leminhhieu98py@gmail.com>
2024-05-16 17:10:15 +0700

    webpack: Environment variables completed

--- webpack.config.js
+++ webpack.config.js
@@ -9,88 +9,92 @@ const BundleAnalyzerPlugin =
 const fontRegex = /\.(woff|woff2|eot|ttf|otf)$/i;
 const imageRegex = /\.(png|svg|jpg|jpeg|gif)$/i;
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  module: {
-    rules: [
-      // Asset management
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: imageRegex,
-        type: 'asset/resource'
-      },
-      {
-        test: fontRegex,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: 'csv-loader'
-      },
-      {
-        test: /\.xml$/i,
-        use: 'xml-loader'
-      },
-      {
-        test: /\.toml/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse
-        }
-      },
-      {
-        test: /\.(yml|yaml)$/i,
-        type: 'json',
-        parser: {
-          parse: yamljs.parse
-        }
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse
-        }
-      }
-    ]
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    port: 1233
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Caching'
-    }),
-    new BundleAnalyzerPlugin()
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: ({ filename }) => {
-      if (fontRegex.test(filename)) return 'fonts/[name][ext]';
-      if (imageRegex.test(filename)) return 'images/[name][ext]';
+module.exports = (env) => {
+  const mode = env.mode === 'production' ? 'production' : 'development';
 
-      return 'asset/[name][ext]';
+  return {
+    mode,
+    entry: './src/index.js',
+    module: {
+      rules: [
+        // Asset management
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: imageRegex,
+          type: 'asset/resource'
+        },
+        {
+          test: fontRegex,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.(csv|tsv)$/i,
+          use: 'csv-loader'
+        },
+        {
+          test: /\.xml$/i,
+          use: 'xml-loader'
+        },
+        {
+          test: /\.toml/i,
+          type: 'json',
+          parser: {
+            parse: toml.parse
+          }
+        },
+        {
+          test: /\.(yml|yaml)$/i,
+          type: 'json',
+          parser: {
+            parse: yamljs.parse
+          }
+        },
+        {
+          test: /\.json5$/i,
+          type: 'json',
+          parser: {
+            parse: json5.parse
+          }
+        }
+      ]
     },
-    clean: true
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+      port: 1233
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        title: 'Environment variables'
+      }),
+      new BundleAnalyzerPlugin()
+    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: ({ filename }) => {
+        if (fontRegex.test(filename)) return 'fonts/[name][ext]';
+        if (imageRegex.test(filename)) return 'images/[name][ext]';
+
+        return 'asset/[name][ext]';
+      },
+      clean: true
+    },
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
         }
       }
     }
-  }
+  };
 };
